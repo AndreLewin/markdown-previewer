276d36e6438255cb4f8cefd32b38bc7187cf8622
axolotl <andre.charles.lewin@gmail.com>
2017-07-08 19:09:10 +0200

    added first components (still empty)

--- src/index.js
+++ src/index.js
@@ -1,3 +1,55 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+
+
+class Input extends React.Component {
+    render() {
+        return (
+            <div>
+                <p>This is the Input part</p>
+                <p>Type something</p>
+            </div>
+        );
+    }
+}
+
+class Output extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            result: "This is the result",
+        };
+    }
+
+
+    render() {
+        return (
+            <div>
+                <p>{this.state.result}</p>
+            </div>
+        );
+    }
+}
+
+
+class App extends React.Component {
+    render() {
+        return (
+            <div>
+                <div>
+                    <Input />
+                </div>
+                <div>
+                    <Output />
+                </div>
+            </div>
+        );
+    }
+}
+
+
+ReactDOM.render(
+    <App />,
+    document.getElementById('root')
+);
